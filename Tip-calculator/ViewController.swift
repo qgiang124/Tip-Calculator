0bfe4fe2958e76921d6218263b6fd6d21109c203
Tran, Giang <gtt0023@my.unt.edu>
2022-08-07 14:34:16 -0500

    minor update

--- Tip-calculator/ViewController.swift
+++ Tip-calculator/ViewController.swift
@@ -18,17 +18,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var TotalAmount: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemYellow
         // Do any additional setup after loading the view.
     }
 
     @IBAction func CalculateTip(_ sender: Any) {
+        //get bill amount from the bill amount text field
         let bill = Double(BillAmountTextField.text!) ?? 0
         
+        //different tip percentages
         let tipPercentages = [0.15, 0.18, 0.2]
         let tip = bill * tipPercentages[TipControl.selectedSegmentIndex]
         let total = bill + tip
         
+        //update tip amount label
         TipAmount.text = String(format: "$%.2f", tip)
+        //update total amount label
         TotalAmount.text = String(format: "$%.2f", total)
     }
 }
